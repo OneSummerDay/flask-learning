fcf5a8ce8fa13f87474b33f6465b2b3af74c16bf
Artem Kraievyi <artem.kraievyi@gmail.com>
2023-11-07 19:49:46 +0200

    feat: added base.html file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route("/")
 def index():
-    return "<h1>Главная страница</h1>"
+    return render_template('index.html')
 
 
-@app.route("about")
+@app.route("/about")
 def about():
-    return ("<h1>Страница о нас</h1>")
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
